7a2dc801530f1397586c6adc396c3bc0ea82532e
yeatom <l980831@163.com>
2022-11-29 06:42:05 +0800

    [feature] add back arrow and top bar hint.

--- component/Header.tsx
+++ component/Header.tsx
@@ -4,12 +4,31 @@ import styled from "styled-components/native";
 export function Header() {
     return (
         <Background source={require('../assets/gradient-background.png')}>
-
+            <TopBar>
+                <TopBarBackButton source={require('../assets/back-button.png')} resizeMode={'contain'}/>
+                <TopBarText>粤康码（广州）</TopBarText>
+            </TopBar>
         </Background>
     )
 }
 
 const Background = styled.ImageBackground`
-  flex: 1;
   height: 555px;
 `;
+
+const TopBar = styled.View`
+  margin-top: 20px;
+  height: 100px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TopBarBackButton = styled.Image`
+  height: 20px;
+`
+
+const TopBarText = styled.Text`
+  font-size: 18px;
+  color: white;
+`

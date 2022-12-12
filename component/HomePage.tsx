7bbffd9923a295ae33a37ca89d16198f59ecb83c
yeatom <l980831@163.com>
2022-12-12 08:22:22 +0800

    [feature] FakeCard and CaringModelButton.

--- component/HomePage.tsx
+++ component/HomePage.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import {ListRenderItemInfo, ScrollView, View} from "react-native";
+import {Image, ImageSourcePropType, ListRenderItemInfo, ScrollView, View} from "react-native";
 import {Header} from "./Header";
 import React, {ElementType} from "react";
 import DataApi, {DetailProps} from "../data/DataApi";
@@ -9,7 +9,10 @@ export default function HomePage() {
         <HomePageContainer>
             <ScrollView>
                 <Header/>
+                <FakeCard source={require('../assets/fake-card-1.jpg')} height={105} marginTop={10}/>
                 <DetailList/>
+                <FakeCard source={require('../assets/fake-card-2.jpg')} height={130} marginTop={-43}/>
+                <CaringModelButton/>
             </ScrollView>
             <FloatingButton/>
         </HomePageContainer>
@@ -52,6 +55,16 @@ function DetailList() {
                 paddingRight: 10,
             }}
         />
+    )
+}
+
+function CaringModelButton() {
+    return (
+        <CaringModelButtonContainer>
+            <CaringModelButtonImage style={{tintColor: '#449bf1'}} source={require('../assets/refresh-button.png')}
+                                    resizeMode={'contain'}/>
+            <CaringModelButtonText>切换至关怀模式</CaringModelButtonText>
+        </CaringModelButtonContainer>
     )
 }
 
@@ -124,4 +137,53 @@ const DetailFlatListItemCustomComponentContainer = styled.View`
   justify-content: center;
   flex: 1;
   padding: 20px;
+`
+
+const FakeCard = (props: { source: ImageSourcePropType, height: number, marginTop?: number }) =>
+    <View
+        style={{
+            width: '92.5%',
+            height: props.height,
+            shadowColor: 'rgba(30,30,30,0.5)',
+            shadowOffset: {width: 0, height: 0},
+            shadowOpacity: 0.1,
+            borderRadius: 7,
+            marginTop: props.marginTop,
+            padding: 1,
+            alignSelf: 'center',
+            backgroundColor: 'white',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+        <Image
+            style={{
+                width: '100%',
+                height: '100%',
+            }}
+            resizeMode={'contain'}
+            source={props.source}/>
+    </View>
+
+const CaringModelButtonContainer = styled.View`
+  flex: 1;
+  height: 50px;
+  border-radius: 3px;
+  justify-content: center;
+  border-color:  #449bf1;
+  align-items: center;
+  flex-direction: row;
+  border-width: 1px;
+  margin: 20px 12px 50px 12px;
+`
+
+const CaringModelButtonImage = styled.Image`
+  height: 25px;
+  width: 25px;
+`
+
+const CaringModelButtonText = styled.Text`
+  color: #449bf1;
+  margin-left: 10px;
+  font-weight: 700;
+  font-size: 17px;
 `

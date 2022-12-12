96a73f5cf5eb47c305dba551ec2b9fbe0ddef7fe
yeatom <l980831@163.com>
2022-12-12 08:31:42 +0800

    [feature] set system brightness when the app launches.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,20 @@
 import HomePage from "./component/HomePage";
+import {useEffect} from "react";
+import * as Brightness from 'expo-brightness';
 
 export default function App() {
+
+    const setBrightness = () => {
+        (async () => {
+            const { status } = await Brightness.requestPermissionsAsync();
+            if (status === 'granted') {
+                await Brightness.setSystemBrightnessAsync(1);
+            }
+        })();
+    };
+
+    useEffect(setBrightness, []);
+
     return <HomePage/>
 }
 
